f3bc8eb9262c82215f15679288f41575d13fce9c
Caranouga <malonepayet@gmail.com>
2024-05-22 20:58:44 +0200

    Update init.sql

--- init.sql
+++ init.sql
@@ -2,14 +2,45 @@ SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
 
+-- Création des tables
+CREATE TABLE `datas` (
+  `id` int(11) NOT NULL,
+  `datetime` datetime NOT NULL,
+  `signalisation` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL CHECK (json_valid(`signalisation`)),
+  `stationnement` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL CHECK (json_valid(`stationnement`)),
+  `feux` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL CHECK (json_valid(`feux`)),
+  `vehicule` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL CHECK (json_valid(`vehicule`)),
+  `depassement` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL CHECK (json_valid(`depassement`)),
+  `orientation` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL CHECK (json_valid(`orientation`)),
+  `priorites` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL CHECK (json_valid(`priorites`)),
+  `conducteur` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL CHECK (json_valid(`conducteur`))
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
 CREATE TABLE `users` (
   `id` int(11) NOT NULL,
   `pseudo` text NOT NULL,
   `password` text NOT NULL,
   `salt` text NOT NULL,
-  `datas` text NOT NULL
+  `data_ids` text DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Index pour la table `datas`
+--
+ALTER TABLE `datas` ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `users`
+--
 ALTER TABLE `users` ADD PRIMARY KEY (`id`);
-ALTER TABLE `users` MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `datas`
+--
+ALTER TABLE `datas` MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT pour la table `users`
+--
+ALTER TABLE `users` MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 COMMIT;
